3b6f8adfe9734af3838ea60eb0e94b41027e4b1b
Paul Mandal <paul@bytemark.co>
2014-10-14 23:34:46 -0400

    Connected UI to MainActivity.onClick()

--- app/src/main/java/mudexample/paulmandal/com/mudexample/MainActivity.java
+++ app/src/main/java/mudexample/paulmandal/com/mudexample/MainActivity.java
@@ -15,11 +15,42 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
 
         // Set up click listeners for everything
+        findViewById(R.id.action_look).setOnClickListener(this);
+        findViewById(R.id.action_inventory).setOnClickListener(this);
+        findViewById(R.id.action_get).setOnClickListener(this);
+        findViewById(R.id.action_drop).setOnClickListener(this);
+        findViewById(R.id.action_north).setOnClickListener(this);
+        findViewById(R.id.action_south).setOnClickListener(this);
+        findViewById(R.id.action_east).setOnClickListener(this);
+        findViewById(R.id.action_west).setOnClickListener(this);
+        findViewById(R.id.action_font_increase).setOnClickListener(this);
+        findViewById(R.id.action_font_decrease).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch(v.getId()) {
+            case R.id.action_look:
+                break;
+            case R.id.action_inventory:
+                break;
+            case R.id.action_get:
+                break;
+            case R.id.action_drop:
+                break;
+            case R.id.action_north:
+                break;
+            case R.id.action_south:
+                break;
+            case R.id.action_east:
+                break;
+            case R.id.action_west:
+                break;
+            case R.id.action_font_increase:
+                break;
+            case R.id.action_font_decrease:
+                break;
+        }
     }
 
 }
