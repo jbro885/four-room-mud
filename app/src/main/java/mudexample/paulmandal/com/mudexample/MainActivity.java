bcb5f86859a22fc46fc118e0aaa11f2ed04950ac
Paul Mandal <paul@bytemark.co>
2014-10-14 23:30:33 -0400

    Added more actions to UI

--- app/src/main/java/mudexample/paulmandal/com/mudexample/MainActivity.java
+++ app/src/main/java/mudexample/paulmandal/com/mudexample/MainActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up click listeners for everything
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
 }
